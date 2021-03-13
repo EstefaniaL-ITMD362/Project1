d5d3ab4d6fe14ec9a0cb9fa5e1477f168e17e4d4
Estefania <elopez17@hawk.iit.edu>
2021-03-12 18:31:02 -0600

    more content

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let inputs = document.getElementById("my-form").elements;
+let dis_name=inputs['submitted-name'];
+let major=nputs['submitted-major'];
+let email=nputs['submitted-email'];
+
+console.log(dis_name.value);
+document.getElementById("rep").innerHTML = window.getElementById("submitted-name").value;
