ed34fbf20d52b9e3bb8eba2e37fa1cb8e3eb405f
Estefania <elopez17@hawk.iit.edu>
2021-04-12 20:48:04 -0400

    updated style

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 function init(){
   var x = document.getElementById("submit"); 
   x.addEventListener('click', function(){
-    const name = document.getElementById('submitted-name').value;
+  const name = document.getElementById('submitted-name').value;
+  var students = []
+  students.push(name);
   sessionStorage.setItem("NAME", name);
+  sessionStorage.setItem("STUDENTS", students);
+
   return;
   });
 }
